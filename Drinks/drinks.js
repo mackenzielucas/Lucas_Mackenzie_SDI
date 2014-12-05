c53680a8db024b471633e07bc61c302e3f48ac6a
Mac <mackenzie@contrastit.net>
2014-12-04 20:19:49 -0800

    Uploaded Drinks.js and added folders

---
+++ Drinks/drinks.js
@@ -0,0 +1,12 @@
+var months = 4;
+var sitespermonth = 2;
+
+console.log("I will drink " + months * 28 + " beers in " + months + " months.");
+
+console.log("I will make " + sitespermonth * months + " sites in " + months + " months.");
+
+var strandspertree = 2;
+var lightsperstrand = 100;
+var numofhouseholds = 122459;
+
+console.log("There are " + numofhouseholds * strandspertree * lightsperstrand + " Christmas light bulbs per household in the U.S. given the number of U.S. households is " + numofhouseholds + " according to U.S. Census Bureau.");
