0a39df0e75f88263fc79b85fd2e5c913216f2fc3
Mac <mackenzie@contrastit.net>
2014-12-04 20:48:35 -0800

    Christmas Equation!
    Christmas Equation!

---
+++ Expressions_Wacky/wacky.js
@@ -0,0 +1,12 @@
+/*
+Mackenzie Lucas
+12/4/14
+Expressions Assignment
+Wacky
+*/
+
+var strandspertree = 2;
+var lightsperstrand = 100;
+var numofhouseholds = 122459;
+
+console.log("There are " + numofhouseholds * strandspertree * lightsperstrand + " Christmas light bulbs per household in the U.S. given the number of U.S. households is " + numofhouseholds + " according to U.S. Census Bureau.");
